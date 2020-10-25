8385de0b1dc50eb4ff42ba103f0741ea8de29308
wwyy <15268131701@163.com>
2020-10-25 20:25:07 +0800

    仿斗鱼直播框架搭建

---
+++ MyDYZB/Classes/Main/MainTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarController.swift
+//  MyDYZB
+//
+//  Created by 王武 on 2020/10/25.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChildVc(storyboard: "Home")
+        addChildVc(storyboard: "Live")
+        addChildVc(storyboard: "Follow")
+        addChildVc(storyboard: "Profile")
+    }
+    
+    private func addChildVc(storyboard: String) {
+        // 1. 通过 storyboard 获取控制器
+        let childVc = UIStoryboard(name: storyboard, bundle: nil).instantiateInitialViewController()!
+
+        // 2. 将childVc作为子控制器
+        addChild(childVc)
+    }
+}
